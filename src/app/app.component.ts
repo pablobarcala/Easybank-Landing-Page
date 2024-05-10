36567cd169a50b24ec9e212c28718c5a9619b1fe
pablobarcala <pablobarcala4@gmail.com>
2024-05-10 12:26:03 -0300

    crar componente de presentation y primeros estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { PresentationComponent } from './components/presentation/presentation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, PresentationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
