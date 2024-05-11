a042f58b3218699cabd801509ee040b027005835
pablobarcala <pablobarcala4@gmail.com>
2024-05-11 10:08:35 -0300

    diseñar componente de why choose easybank

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { PresentationComponent } from './components/presentation/presentation.component';
+import { WhyComponent } from './components/why/why.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, PresentationComponent],
+  imports: [RouterOutlet, HeaderComponent, PresentationComponent, WhyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
