82095c8332cf455b9642d5e0405232c8263985b1
pablobarcala <pablobarcala4@gmail.com>
2024-05-11 11:02:21 -0300

    primeros estilos de los articulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { PresentationComponent } from './components/presentation/presentation.component';
 import { WhyComponent } from './components/why/why.component';
+import { ArticlesComponent } from './components/articles/articles.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, PresentationComponent, WhyComponent],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent, 
+    PresentationComponent, 
+    WhyComponent,
+    ArticlesComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
