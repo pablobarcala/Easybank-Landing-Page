1bd85c66ece6b78e968dda1a6c201b5cc2b40b71
pablobarcala <pablobarcala4@gmail.com>
2024-05-12 20:47:25 -0300

    agregar firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    apiKey: "AIzaSyBMVYQP8TXuL71G50L7lTmavT57sfGM0fc",
+    authDomain: "easybank-dfcd9.firebaseapp.com",
+    projectId: "easybank-dfcd9",
+    storageBucket: "easybank-dfcd9.appspot.com",
+    messagingSenderId: "738282916587",
+    appId: "1:738282916587:web:35320657348e29d36b8cae"
+}
