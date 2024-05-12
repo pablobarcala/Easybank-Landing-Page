751dd82af043f880ac0b8ad57b603478ecca8266
pablobarcala <pablobarcala4@gmail.com>
2024-05-11 21:03:41 -0300

    agregar contenido y estilizar componente de footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { PresentationComponent } from './components/presentation/presentation.component';
 import { WhyComponent } from './components/why/why.component';
 import { ArticlesComponent } from './components/articles/articles.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ import { ArticlesComponent } from './components/articles/articles.component';
     HeaderComponent, 
     PresentationComponent, 
     WhyComponent,
-    ArticlesComponent
+    ArticlesComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
